3e5387ce2c17899a14b5f90d6b284d50ea5aa197
dang461 <142491780+dang461@users.noreply.github.com>
2023-09-07 18:08:17 +0700

    upcode

--- HTML/Newapp/Program.cs
+++ HTML/Newapp/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("xinchao");
+using System.Runtime.CompilerServices;
+
+Console.writeline("Hello, world!");
+Console.writeline("LeHaiDang");
+
+//khai bao ho va ten 
+const string hoTen = "Le Hai Dang";
+const int tuoi = 23;
+Console.writeline("{0} , {1}", hoTen, tuoi);
+
