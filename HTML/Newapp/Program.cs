2f5432d311daa3e231f7a0901b178a31b15c3d28
dang461 <142491780+dang461@users.noreply.github.com>
2023-09-08 20:05:08 +0700

    upcode

--- HTML/Newapp/Program.cs
+++ HTML/Newapp/Program.cs
@@ -6,14 +6,6 @@
     int b; 
     System.Console.Write("gia tri cua b la: ");
     b=Convert.ToInt32(Console.ReadLine());
-    int c=a-b;
-    int d=a*b;
-    int e=a/b;
-    int f=a%b;
-    System.Console.WriteLine("gia tri cua c= " +c);
-    System.Console.WriteLine("gia tri cua d= " +d);
-    System.Console.WriteLine("gia tri cua e= " +e,"vu phan du la: " +f);
-    System.Console.WriteLine("va phan du la: " +f);
     if (a>b){
 
         System.Console.Write("a lon hon b ");
